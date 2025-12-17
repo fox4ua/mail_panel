b91dcf547e5066470675b847b87d42636fdd7477
Aleksandr <fox4ua@users.noreply.github.com>
2025-12-17 11:30:36 +0200

    Restore Module Center name metadata

--- modules/System/ModuleCenter/Config/Info.php
+++ modules/System/ModuleCenter/Config/Info.php
@@ -4,6 +4,7 @@ namespace Modules\System\ModuleCenter\Config;
 
 class Info
 {
+    public string $name    = 'modulecenter';
     public string $title   = 'Module Center';
     public string $version = '1.1.0';
     public int    $weight  = 0;
