ebaaf8ab2299927326b85030d8977e3d6c3f0add
Aleksandr <fox4ua@users.noreply.github.com>
2025-12-17 11:14:34 +0200

    Simplify ModuleCenter info config

--- modules/System/ModuleCenter/Config/Info.php
+++ modules/System/ModuleCenter/Config/Info.php
@@ -4,8 +4,10 @@ namespace Modules\System\ModuleCenter\Config;
 
 class Info
 {
-    public string $name    = 'modulecenter';
     public string $title   = 'Module Center';
     public string $version = '1.1.0';
     public int    $weight  = 0;
+
+    /** @var string[] */
+    public array $requires = ['system/menu', 'system/layout', 'system/core'];
 }
